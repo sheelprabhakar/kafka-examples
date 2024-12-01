252b46aaa917395280228907b1810614fc3ecfe1
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2024-12-01 17:38:37 +0530

    update to spring boot 3 and java 21

--- weather-agents-simulator/src/main/java/c4c/kafka/example/weatheragent/config/KafkaProducerConfig.java
+++ weather-agents-simulator/src/main/java/c4c/kafka/example/weatheragent/config/KafkaProducerConfig.java
@@ -31,6 +31,9 @@ public class KafkaProducerConfig {
         configProps.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 JsonSerializer.class);
+        configProps.put(
+                ProducerConfig.ACKS_CONFIG,
+                "1");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
